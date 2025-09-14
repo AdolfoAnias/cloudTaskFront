be438858e6de1ee9bee44db19e056ae1a56f2f24
AdolfoAnias <adolfoanias@gmail.com>
2025-09-14 07:58:50 -0400

    Ajustes en rutas

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,12 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'), // tu layout con el <router-view>
+    component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', redirect: '/tasks' }, // redirige la ruta raíz a /tasks
+      { path: '', redirect: '/tasks' },
       { path: 'tasks', component: () => import('pages/TaskList.vue'), name: 'tasks' },
       { path: 'keywords', component: () => import('pages/KeywordList.vue'), name: 'keywords' },
-      // otras rutas...
     ],
   },
   {
@@ -18,58 +17,6 @@ const routes = [
   },
 ]
 
-/*
-const routes = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
-  },
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: 'mi-componente', component: () => import('pages/MyComponentPage.vue') }],
-  },
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: 'login', component: () => import('pages/LoginPage.vue') }],
-  },
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: 'home', component: () => import('pages/HomePage.vue'), meta: { requiresAuth: true } },
-    ],
-  },
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      {
-        path: 'tasks',
-        component: () => import('pages/TaskList.vue'),
-        meta: { requiresAuth: true },
-      },
-    ],
-  },
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      {
-        path: 'keywords',
-        component: () => import('pages/KeywordList.vue'),
-        meta: { requiresAuth: true },
-      },
-    ],
-  },
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
-  },
-]
-*/
 const router = createRouter({
   history: createWebHistory(),
   routes,
